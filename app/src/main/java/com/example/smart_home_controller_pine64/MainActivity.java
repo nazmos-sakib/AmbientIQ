1019052dee8a80f7236967c637aae6b5f5d875ba
naz <naz@sakib>
2024-01-12 14:34:26 +0100

    first commit

---
+++ app/src/main/java/com/example/smart_home_controller_pine64/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.smart_home_controller_pine64;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
